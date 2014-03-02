36118b6bab29cd4d824dfcda49b538d1996bc374
Anabelle Handdoek <github@huellaspyp.com>
2014-03-02 18:32:03 -0500

    define tableros

---
+++ api/models/Tablero.js
@@ -0,0 +1,22 @@
+/**
+ * Tablero.js
+ *
+ * @description :: Los taleros de colaborativo, define si estan activos y sus fuentes.
+ * @docs		:: http://sailsjs.org/#!documentation/models
+ */
+
+module.exports = {
+
+	attributes: {
+           name: {
+            type: 'STRING',
+            required: true
+           },
+           active: {
+            type: 'BOOLEAN',
+            defaultsTo: true,
+            required: true
+           }
+	}
+
+};
