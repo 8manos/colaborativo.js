264c2fc7eb9bdbc5cab33aa2b9f99b9fdd0fd3d9
Anabelle Handdoek <github@huellaspyp.com>
2014-03-09 19:47:56 -0500

    add optional packery directive

--- assets/js/app/app.js
+++ assets/js/app/app.js
@@ -1,9 +1,36 @@
 var app = angular.module('colaborativo', [ 'ngResource', 'colaborativo.controllers' ]);
 
+
+// Factories
 app.factory('Tablero', ['$resource', function($resource){
     return $resource('/tablero/:id', {id:'@id'});
 }]);
 
 app.factory('Publicacion', ['$resource', function($resource){
     return $resource('/publicacion/entablero/:id', {id:'@id'}, { get: { method:'GET', params:{ id:''}, isArray:true } });
+}]);
+
+
+// Directives
+var dannyPackery = app.directive('dannyPackery', ['$rootScope', function($rootScope) {
+    return {
+        restrict: 'A',
+        link: function(scope, element, attrs) {
+            console.log('Running dannyPackery linking function!');
+            if($rootScope.packery === undefined || $rootScope.packery === null){
+                console.log('making packery!');
+                $rootScope.packery = new Packery(element[0].parentElement, {
+                													columnWidth: '.item',
+                													gutter: 0
+                												});
+                $rootScope.packery.bindResize();
+                $rootScope.packery.prepended(element[0]);
+                $rootScope.packery.items.splice(1,1); // hack to fix a bug where the first element was added twice in two different positions
+            }
+            else{
+                $rootScope.packery.prepended(element[0]);
+            }
+            $rootScope.packery.layout();
+        }
+    };
 }]);
