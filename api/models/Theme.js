8ccbb9d0566637233dbede2cdbf396ad4e5ceb85
Anabelle Handdoek <github@huellaspyp.com>
2014-03-26 19:31:26 -0500

    create theme model

---
+++ api/models/Theme.js
@@ -0,0 +1,47 @@
+/**
+ * Theme.js
+ *
+ * @description :: Theme para el front end de los tableros.
+ * @docs		:: http://sailsjs.org/#!documentation/models
+ */
+
+module.exports = {
+
+	attributes: {
+		name: {
+			type: 'STRING',
+			required: true
+		},
+		slug: {
+			type: 'STRING',
+			required: true
+		},
+		header_bg_color: {
+			type: 'STRING',
+			maxLength: 7,
+			required: false
+		},
+		header_bg_url: {
+			type: 'STRING',
+			required: false
+		},
+		header_bg_size: {
+			type: 'STRING',
+			required: false
+		},
+		body_bg_color: {
+			type: 'STRING',
+			maxLength: 7,
+			required: false
+		},
+		body_bg_url: {
+			type: 'STRING',
+			required: false
+		},
+		body_bg_size: {
+			type: 'STRING',
+			required: false
+		}
+	}
+
+};
