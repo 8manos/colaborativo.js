adb179ccc5a0f4af1a8c0910f4f2eb26bff6bd1d
MaoMantish <mauricio@8manos.com>
2014-03-31 18:45:27 -0500

    basic authentication using passport
    http://blog.thesparktree.com/post/77311774912/creating-a-sails-application-using-passport

---
+++ api/models/User.js
@@ -0,0 +1,12 @@
+module.exports = {
+
+  attributes: {
+    email: {
+      type: 'email'
+    },
+    password: {
+      type: 'string'
+    }
+  }
+
+};
---
+++ api/policies/isAuthenticated.js
@@ -0,0 +1,11 @@
+module.exports = function(req, res, next) {
+
+  // User is allowed, proceed to the next policy,
+  // or if this is the last policy, the controller
+  if (req.isAuthenticated()) {
+    return next();
+  }
+
+  // User is not allowed
+  return res.redirect('/user/login');
+};
