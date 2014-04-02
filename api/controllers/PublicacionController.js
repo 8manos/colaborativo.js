8bc0c89d4abb41b9e2fb46e0345b335cf1bf3054
Anabelle Handdoek <github@huellaspyp.com>
2014-04-01 20:06:54 -0500

    excluir retweets en primera carga

--- api/controllers/PublicacionController.js
+++ api/controllers/PublicacionController.js
@@ -14,7 +14,7 @@ module.exports = {
 		Tablero.findByID(id, function ( tablero ) {
 
 			if ( tablero ) {
-				Publicacion.find({ entablero: tablero.id }).limit( 50 ).sort({ createdAt: 'desc' }).done( function( err, publicaciones ){
+				Publicacion.find({ entablero: tablero.id }).where({"data.retweeted_status": {"$exists": false }}).limit( 50 ).sort({ createdAt: 'desc' }).done( function( err, publicaciones ){
 					if( publicaciones ){
 						publicaciones = publicaciones.reverse();
 
