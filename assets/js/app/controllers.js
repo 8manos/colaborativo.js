3b1ed6ee46443044065b5319612ac97c896c4301
Anabelle Handdoek <github@huellaspyp.com>
2014-09-25 18:59:30 -0500

    cleanup SingleCtrl

--- assets/js/app/controllers.js
+++ assets/js/app/controllers.js
@@ -81,8 +81,6 @@ controllers.controller('TableroCtrl', function ($scope, $ocModal, $attrs, $sails
 controllers.controller('SingleCtrl', function ($scope, $init, $sails) {
 
   (function () {
-  	$scope.publicacion = [];
-  	$scope.publicacion = $init.post;
   	console.log( "SingleCtrl", $scope.publicacion );
 	/* $sails.get("/tablero", function (data) {
 	  $scope.tableros = data;
