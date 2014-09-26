7def5f34ce27f6069f7d0bbf8748ffece7f66798
MaoMantish <mauricio@8manos.com>
2014-09-26 16:14:33 -0500

    callback url de twitter automatica. passport movido a servicios

--- api/policies/isAuthenticated.js
+++ api/policies/isAuthenticated.js
@@ -1,5 +1,6 @@
-module.exports = function(req, res, next) {
+module.exports = function isAuthenticated(req, res, next) {
 
+  console.log('req', req);
   // User is allowed, proceed to the next policy,
   // or if this is the last policy, the controller
   if (req.isAuthenticated()) {
--- api/services/passport.js
+++ api/services/passport.js
@@ -2,13 +2,14 @@ var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 var TwitterStrategy = require('passport-twitter').Strategy;
 var passwordHash = require('password-hash');
+var url = require('url');
 
 passport.serializeUser(function(user, done) {
   done(null, user.id);
 });
 
 passport.deserializeUser(function(id, done) {
-  User.findOneById(id).done(function (err, user) {
+  User.findOneById(id).exec(function (err, user) {
     done(err, user);
   });
 });
@@ -18,7 +19,7 @@ passport.use(new LocalStrategy({
     passwordField: 'password'
   },
   function(email, password, done) {
-    User.findOne({ email: email}).done(function(err, user) {
+    User.findOne({ email: email}).exec(function(err, user) {
       if (err) { return done(err); }
       if (!user) { return done(null, false, { message: 'Unknown user ' + email }); }
       if ( ! passwordHash.verify(password, user.password) ) { return done(null, false, { message: 'Invalid password' }); }
@@ -30,7 +31,7 @@ passport.use(new LocalStrategy({
 passport.use(new TwitterStrategy({
     consumerKey: process.env.T_CONSUMER_KEY,
     consumerSecret: process.env.T_CONSUMER_SECRET,
-    callbackURL: "http://127.0.0.1:5000/user/twitter/callback"
+    callbackURL: url.resolve(sails.getBaseurl(), '/user/twitter/callback')
   },
   function(token, tokenSecret, profile, done) {
     User.findOrCreate({
@@ -46,3 +47,5 @@ passport.use(new TwitterStrategy({
     });
   }
 ));
+
+module.exports = passport;
