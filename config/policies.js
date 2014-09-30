9b8e4f2f6645dbc16176422525acb51904d59aff
Anabelle Handdoek <github@huellaspyp.com>
2014-09-30 10:24:15 -0500

    un hueco de seguridad tan grande como la app misma

--- config/policies.js
+++ config/policies.js
@@ -16,7 +16,7 @@ module.exports.policies = {
 
   // Default policy for all controllers and actions
   // (`true` allows public access)
-  '*': ['isAuthenticated', 'isAdmin'],
+  '*': true, // ['isAuthenticated', 'isAdmin'],
   'home': true,
   'tablero': {
   	'findOne': true,
